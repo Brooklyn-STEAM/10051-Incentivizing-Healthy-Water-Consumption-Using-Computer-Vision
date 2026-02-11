55c190cd693ff0314a375ca0c7a52ebfa5edf029
Valerie <vburke@brooklynsteamcenter.org>
2026-02-11 10:45:50 -0500

    login (#6)

--- main.py
+++ main.py
@@ -9,10 +9,49 @@ app = Flask(__name__)
 
 config = Dynaconf(settings_file = ["settings.toml"])
 
+login_manager = LoginManager( app )
+
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
 
+login_manager.login_view = '/login'
+class User:
+    is_authenticated = True
+    is_active = True
+    is_anonymous = False
+
+    def __init__(self, result):
+        self.name = result['Name']
+        self.email = result['Email']
+        self.username = result['Username']
+        self.id = result['ID']
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        connection = connect_db()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM `User` WHERE `Username` = %s" , (username) )
+        result = cursor.fetchone()
+        connection.close()
+        if result is None:
+            flash("No user found")
+        elif password != result["Password"]:
+            flash("Incorrect password")
+        else:
+            login_user(User(result))
+            return redirect('/browse')
+    return render_template("login.html.jinja")
+
+@app.route("/logout",  methods = ["POST", "GET"])
+@login_required
+def logout():
+    logout_user()
+    flash("You Have Been Logged Out! Thanks For Shopping")
+    return redirect("/login")
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
